3c0f0c3b7a0c7d54a1ce4afee1831d63d57a0a28
htsiang <htsiang3@gmail.com>
2019-07-27 01:45:28 -0700

    Updates

---
+++ precache-manifest.2770049b6ed92de1083a35404d73850c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98c34459d7d530129b025633eefcac5",
+    "url": "/clicky-game.github.io/index.html"
+  },
+  {
+    "revision": "37f7582ac621d80cc297",
+    "url": "/clicky-game.github.io/static/css/main.5ae79994.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/clicky-game.github.io/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "37f7582ac621d80cc297",
+    "url": "/clicky-game.github.io/static/js/main.dcc034c5.chunk.js"
+  },
+  {
+    "revision": "2e9e45a4865fe8c68eea",
+    "url": "/clicky-game.github.io/static/js/runtime~main.4be478d2.js"
+  },
+  {
+    "revision": "c50d1e40ba8e05a798a87fe8ab47e8a3",
+    "url": "/clicky-game.github.io/static/media/10.c50d1e40.jpg"
+  },
+  {
+    "revision": "ede25ce251c04ab2a4027da1e233dcd6",
+    "url": "/clicky-game.github.io/static/media/12.ede25ce2.gif"
+  },
+  {
+    "revision": "2b83d982af67d68f40096327c04b8916",
+    "url": "/clicky-game.github.io/static/media/3.2b83d982.gif"
+  },
+  {
+    "revision": "742dddc92bf35eb3cbd96b1e4d63889f",
+    "url": "/clicky-game.github.io/static/media/4.742dddc9.gif"
+  },
+  {
+    "revision": "6faa031d1ebfe1067da18f8f38924f49",
+    "url": "/clicky-game.github.io/static/media/5.6faa031d.gif"
+  },
+  {
+    "revision": "c2b648ebd65451c2b762f8bc5cd379fd",
+    "url": "/clicky-game.github.io/static/media/6.c2b648eb.gif"
+  },
+  {
+    "revision": "f22b003cc9c09ee668f3ccf96fb97820",
+    "url": "/clicky-game.github.io/static/media/8.f22b003c.gif"
+  },
+  {
+    "revision": "0114d5f7f74007a80e098d186f55a5e1",
+    "url": "/clicky-game.github.io/static/media/9.0114d5f7.gif"
+  },
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/clicky-game.github.io/static/media/react-logo-transparent.692adb35.png"
+  }
+]);
